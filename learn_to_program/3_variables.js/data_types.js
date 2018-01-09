a124b90359f83bbd3fc97b0ad20d9e420990ab37
Richard Chernanko <rchernanko@hotmail.co.uk>
2018-01-09 08:50:42 +0000

    More notes on data types

--- learn_to_program/3_variables.js/data_types.js
+++ learn_to_program/3_variables.js/data_types.js
@@ -42,4 +42,60 @@ console.log(typeof result);
 console.log(typeof console);
 // Prints out 'object'
 
-//TODO up to 6 mins...
+/*
+But this is where things start to get interesting...
+What if I do an operation on 2 different data types?
+*/
+var number1 = 3;
+var string1 = '3';
+var answer = number1 + string1;
+console.log(answer);
+console.log(typeof answer);
+/*
+The value of answer will be '33' and the type will be 'string'.
+
+What's happened? Well, in 'number1 + string1' Javascript sees that one of the
+types is a string and so assumes you want to concatenate a string
+*/
+
+/*
+Another example, but instead of using addition, let's use multiply. There is a
+slightly different result!
+*/
+var number2 = 3;
+var string2 = '2.5';
+var answer = number2 * string2;
+console.log(answer);
+console.log(typeof answer);
+/*
+Here, I get the answer of 7.5 (3 x 2.5). So while with an addition sign, JS
+thinks I want to concatenate two strings, when we use a multiply sign, JS thinks
+that we must want to multiple 2 numbers.
+
+Interesting - If I were to change string2 to e.g. 'hello', console.log(answer)
+would return NaN (not a number), andd the typeof answer would be number.
+
+I.E. be very careful when using different data types.
+
+*/
+
+/*
+One built in command we can use to convert the string '3' to the number 3, is
+parseInt
+*/
+
+var number3 = 5;
+var string3 = '3';
+var result = number3 + string3;
+console.log(result);
+console.log(typeof result);
+//At present, this would print '53' and would be a string. But using parseInt...
+
+var number4 = 5;
+var string4 = '3';
+var result = number4 + parseInt(string4);
+console.log(result);
+console.log(typeof result);
+//Because I've used the parseInt function, the result will be 8 and will be a number
+
+console.log(parseInt('hello'));
