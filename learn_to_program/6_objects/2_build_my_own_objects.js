2d71397853ce16eefc042d28fde2a5eade8a5340
rchernanko <rchernanko@hotmail.co.uk>
2018-01-26 20:50:38 +0000

    Building my own objects

---
+++ learn_to_program/6_objects/2_build_my_own_objects.js
@@ -0,0 +1,11 @@
+//Defining my own object. 
+//Several approaches. 
+//Below is one example - is called the 'object literal' - creates objects on demand:
+
+var die = {
+
+};
+
+console.log(die);
+
+//UP to 1 minute
